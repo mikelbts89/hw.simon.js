f2b5636dcbd943a1309361b57d9d3e31f5feb785
mikelbts89 <mikelbts89@gmail.com>
2020-12-06 21:40:04 +0200

    add_some_new_way_of_writining_arguments

--- homeWork.30.11.2020/app.js
+++ homeWork.30.11.2020/app.js
@@ -1,11 +1,11 @@
 // /////////////////// Ex.1 /////////////////////////
 
 // let users = [
-//   { name: "Mikel" },
-//   { name: "Anna" },
-//   { name: "Thomas" },
-//   { name: "Charlie" },
-//   { name: "Sem" },
+//   { name: "Mikel", lastName:"Bugayets", },
+//   { name: "Anna", lastName:"Bugayets", },
+//   { name: "Thomas", lastName:"Bugayets", },
+//   { name: "Charlie", lastName:"Bugayets", },
+//   { name: "Sem", lastName:"Bugayets", },
 // ];
 
 // let findUser = (userName) => {
@@ -77,53 +77,125 @@
 
 ////////////////////// Ex.4 ///////////////////////////
 
-let getMyCordinates = () => {
-  return new Promise((resolve, reject) => {
-   setTimeout(() => {
-    if (navigator.geolocation) {
-        resolve(navigator.geolocation.getCurrentPosition(showPosition));
-      } else {
-        reject("Geolocation is not supported by this browser.");
-      }
-   }, 2000);
+// let getMyCordinates = () => {
+//   return new Promise((resolve, reject) => {
+//    setTimeout(() => {
+//     if (navigator.geolocation) {
+//         resolve(navigator.geolocation.getCurrentPosition(showPosition));
+//       } else {
+//         reject("Geolocation is not supported by this browser.");
+//       }
+//    }, 2000);
 
-    function showPosition(position) {
-      console.log(
-        "Latitude: " +
-          position.coords.latitude +
-          "\nLongitude: " +
-          position.coords.longitude
-      );
-    }
-  });
-};
+//     function showPosition(position) {
+//       console.log(
+//         "Latitude: " +
+//           position.coords.latitude +
+//           "\nLongitude: " +
+//           position.coords.longitude
+//       );
+//     }
+//   });
+// };
 
-// getMyCordinates()
-//   .then((a) => a)
-//   .catch((b) => console.log(b));
+// // getMyCordinates()
+// //   .then((a) => a)
+// //   .catch((b) => console.log(b));
 
 /////////////////////////////////////////////////////////////////
 
 //////////////////////// Ex.5 ////////////////////////////////
 
-let numArr = [];
+// let numArr = [];
 
-for (let i = 0; i < 20; i++) {
-  let randomNumber = Math.floor(Math.random() * 100) + 1;
-  numArr.push(randomNumber);
-}
-console.log(numArr);
+// for (let i = 0; i < 20; i++) {
+//   let randomNumber = Math.floor(Math.random() * 100) + 1;
+//   numArr.push(randomNumber);
+// }
+// console.log(numArr);
 
-let multiplyNumbers = (num) => {
-  return new Promise((resolve) => {
-      let newNumArr = numArr.map((item) => item * num);
-      setTimeout(() => {
-      resolve(newNumArr);
-    }, 1000);
-  });
-};
-Promise.all([getMyCordinates(),multiplyNumbers(2)]).then(item => { return[
-    console.log(item[1]) , item[0]
-] })
+// let multiplyNumbers = (num) => {
+//   return new Promise((resolve) => {
+//       let newNumArr = numArr.map((item) => item * num);
+//       setTimeout(() => {
+//       resolve(newNumArr);
+//     }, 1000);
+//   });
+// };
+// Promise.all([getMyCordinates(),multiplyNumbers(2)]).then(item => { return[
+//     console.log(item[1]) , item[0]
+// ] })
 
-////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+// /////////////////////// Ex.1 async ////////////////////////////
+
+// let users = [
+//   { name: "Mikel", lastName: "Bugayets" },
+//   { name: "Anna", lastName: "Bugayets" },
+//   { name: "Thomas", lastName: "Bugayets" },
+//   { name: "Charlie", lastName: "Bugayets" },
+//   { name: "Sem", lastName: "Bugayets" },
+// ];
+
+// let findUserById = async (userName) => {
+//   try {
+//     let result = await users.find((user) => user.name === userName);
+//     return result;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// findUserById("Mikel").then(a=>console.log(a))
+
+// ///////////////////////////////////////////////////////////////////
+
+// ////////////////////////// Ex.2 async //////////////////////////
+
+// let numArr = [];
+
+// for (let i = 0; i < 20; i++) {
+//   let randomNumber = Math.floor(Math.random() * 100) + 1;
+//   numArr.push(randomNumber);
+// }
+
+// let getNumbers = async (num) => {
+//   try {
+//     let fillteredNumbers = await numArr.filter((number) => number > num);
+//     return fillteredNumbers;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// getNumbers(34).then((a) => console.log(a));
+
+// ////////////////////////////////////////////////////////////
+
+// let getLocation = () => {
+//   let geolocation = navigator.geolocation;
+//   geolocation.getCurrentPosition(showLocation, errorHandler);
+// };
+
+// let showLocation = (position) => {
+//   let latitude = position.coords.latitude;
+//   let longitude = position.coords.longitude;
+//   console.log(latitude, longitude);
+// };
+
+// let errorHandler = (err) => {
+//   if (err.code == 1) {
+//     console.log(err.code, err);
+//   }
+// };
+
+// ///////////////////////////////////////////////////////
+
+// let someFunction = ({ name, age, color, country }) => {
+//   console.log(name, age, color, country);
+// };
+
+// someFunction({ name: "mikel", age: 12, color: "red", country: "israel" });
+
+// ////////////////////////////////////////////////////////////
